6832e1e88f4b8f352f48721b859adf290efe564e
Enzo <enhance674@gmail.com>
2025-06-25 15:02:27 +0200

    Initial Commit

---
+++ src/main/java/org/example/api_biodiversite/ApiBiodiversiteApplication.java
@@ -0,0 +1,13 @@
+package org.example.api_biodiversite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBiodiversiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBiodiversiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/api_biodiversite/ApiBiodiversiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.api_biodiversite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBiodiversiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
